c6a3e7b42ba76e658f08d1f8a512ff6a39e36d02
Dan <dan.geffroy@capgemini.com>
2016-06-28 17:36:08 +0200

    prototyping :)

--- src/app/big-pics.component.ts
+++ src/app/big-pics.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['big-pics.component.css']
 })
 export class BigPicsAppComponent {
-  title = 'big-pics works!';
+   @Input() input = 'nature';
 }
