0654e48a85fd2dfe2d00a9c0b14022604bc9085e
Dan <dan.geffroy@capgemini.com>
2016-07-04 16:49:10 +0200

    building on prod

---
+++ dist/system-config.js
@@ -0,0 +1,52 @@
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {
+    "jquery": "vendor/jquery",
+    "materialize": "vendor/materialize-css"
+};
+/** User packages configuration. */
+var packages = {
+    'materialize': {
+        "format": "global",
+        "main": "dist/js/materialize",
+        "defaultExtension": "js"
+    }
+};
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+    'app/side-slide',
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
